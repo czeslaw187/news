48defa2cebfffb9a637d2ab08623a5106fe38d7c
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-10-28 18:31:05 +0100

    authorization bearer

--- pages/api/getCurrencyNews.js
+++ pages/api/getCurrencyNews.js
@@ -4,7 +4,11 @@ export default async function getCurrency(req, res) {
     if (req.body){
         const {currency} = req.body
         try {
-            let response = await axios.get(`https://newsapi.org/v2/everything?q=${currency}&apiKey=${process.env.NEXT_PUBLIC_NEWSAPI_KEY}`)
+            let response = await axios.get(`https://newsapi.org/v2/everything?q=${currency}`,{
+                headers: {
+                    Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWSAPI_KEY}`
+                }
+            })
             res.json(response.data)
         } catch (error) {
             return res.json({message: error.message})

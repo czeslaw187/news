4b94528a35c7ab54783107438db70d114258f282
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-10-29 00:13:35 +0100

    dynamic navbar list

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,20 +5,25 @@ import { activePage } from "../lib/newsSlice";
 function Navbar() {
     const dispatch = useDispatch()
     const whatsActive = useSelector(state=>state.news.pageActive)
+    const pages = [
+        {title: 'News', route: '/myNews'},
+        {title: 'Home', route: '/'}
+    ]
     console.log(whatsActive, 'navbar')
     return ( 
         <div className="w-full h-[3rem] bg-gradient-to-br from-lime-200 py-3">
             <ul className="flex flex-row-reverse pl-3 justify-end">
-                <li className="px-3">
-                    <Link href={'/myNews'}>
-                        <a href='#' id="news" onClick={(e)=>{dispatch(activePage(e.target.id))}} className={whatsActive == 'news' ? 'underline' : 'no-underline'}>News</a>
-                    </Link>
-                </li>
-                <li className="px-3">
-                    <Link href={'/'}>
-                        <a href='#' id="home" onClick={(e)=>{dispatch(activePage(e.target.id))}} className={whatsActive == 'home' ? 'underline' : 'no-underline'}>Home</a>
-                    </Link>
-                </li>
+                {
+                    pages.map((el,id)=>{
+                        return (
+                            <li key={id} className="px-3">
+                                <Link href={el.route}>
+                                    <a href='#' id={el.title} onClick={(e)=>{dispatch(activePage(e.target.id))}} className={whatsActive == el.title ? 'underline' : 'no-underline'}>{el.title}</a>
+                                </Link>
+                            </li>
+                        )
+                    })
+                }
             </ul>
         </div>
      );

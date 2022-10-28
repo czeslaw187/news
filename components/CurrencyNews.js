e4ac2fafe040cfb46d19755dc9856e0bdfb779c6
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-10-28 16:53:46 +0100

    added new component to render list of news

---
+++ components/CurrencyNews.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+function CurNews({item}) {
+    const date = new Date(item.publishedAt)
+    return ( 
+        <li className="w-11/12 border-2 rounded-md shadow-xl h-fit my-3 mx-auto">
+            <h1 className="text-xl border-b-gray-900 border-2">{item.title}</h1>
+            <p>Source: <span className="font-bold">{item.source.Name}</span></p>
+            <p>Date released: {date.toLocaleString()}</p>
+            <p className="hover:text-blue-600">URL: <Link href={item.url}>{item.url}</Link></p>
+        </li>
+     );
+}
+
+export default CurNews;
--- pages/myNews.js
+++ pages/myNews.js
@@ -1,17 +1,25 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchCurrencyNews } from "../lib/newsSlice";
+import CurNews from "../components/CurrencyNews";
 
 function myNews() {
     const dispatch = useDispatch()
+    const currency = useSelector((state)=>state.news.currencyNews)
+
     useEffect(()=>{
-        dispatch(fetchCurrencyNews('australian'))
-    },[fetchCurrencyNews])
-    const currency = useSelector(state=>state.currencyNews)
+        dispatch(fetchCurrencyNews('australian dollar'))
+    },[])
     console.log(currency, 'surrency')
     return ( 
-        <div className="min-h-screen w-full pl-5">
-            <h3>My News</h3>
+        <div className="min-h-screen w-full pl-5 pt-5">
+            <ul>
+                {
+                    currency ? currency.map((el, id)=>{
+                        return <CurNews key={id} item={el} id={id}/>
+                    }): null
+                }
+            </ul>
         </div>
      );
 }
